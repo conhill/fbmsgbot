361866cc2b56800c544b6063e9005a8972004f37
Connor Hill <conhill@optonline.net>
2017-07-09 17:36:26 -0400

    3

--- app.js
+++ app.js
@@ -193,6 +193,7 @@ app.post('/webhook', function (req, res) {
     var events = req.body.entry[0].messaging;
     for (i = 0; i < events.length; i++) {
         var event = events[i];
+        res.send(event.message.text);
         // if (event.message && event.message.text) {
         //     sendMessage(event.sender.id, {text: "Echo: " + event.message.text});
         // }
